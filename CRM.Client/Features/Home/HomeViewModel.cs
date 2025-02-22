398484a0e0cd37faa3a321477e41a18b907204ef
Tung Phan <nyanedm@gmail.com>
2025-02-22 13:03:13 +0700

    update

--- CRM.Client/Features/Home/HomeViewModel.cs
+++ CRM.Client/Features/Home/HomeViewModel.cs
@@ -10,7 +10,7 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CRM.Library.Entities;
 using Telerik.Windows.Documents.Fixed.Model.Data;
 using Telerik.Windows.Documents.Spreadsheet.Expressions.Functions;
-
+using CRM.UI;
 namespace CRM.Client.Features.Home
 {
     public partial class HomeViewModel : ObservableObject
@@ -18,23 +18,73 @@ namespace CRM.Client.Features.Home
         [ObservableProperty]
         private DataTable data;
 
-        private ObservableCollection<Flight> roles;
+        private ObservableCollection<Role> roles;
 
-        public ObservableCollection<Flight> Roles => roles ?? (roles = CreateRoles());
+        public ObservableCollection<Role> Roles => roles ?? (roles = CreateRoles());
 
-        private ObservableCollection<Flight> CreateRoles()
+        private ObservableCollection<Role> CreateRoles()
         {
-            return new ObservableCollection<Flight>
+            return new ObservableCollection<Role>
             {
-                new Flight()
+                new Role()
+                {
+                    Logo = string.Format(FaRegModels.AppleCore),
+                    CompanyName = "Apple",
+                    Position = "Visual designer",
+                    Duration = "Full time",
+                    JobId = "123123123",
+                    Status = "Phone interview",
+                    Activity = "",
+                },
+                new Role()
                 {
                     Logo = "",
-                    CompanyName = "Apple",
-                    Position = "A",
-                    Duration = "A",
-                    JobId = "S",
-                    Status = "S",
-                    Activity = "S",
+                    CompanyName = "Booster",
+                    Position = "Product designer",
+                    Duration = "Full time",
+                    JobId = "123123123",
+                    Status = "Applied",
+                    Activity = "",
+                },
+                new Role()
+                {
+                    Logo = "",
+                    CompanyName = "Goodle",
+                    Position = "Interactive designer",
+                    Duration = "Full time",
+                    JobId = "123123123",
+                    Status = "Zoom call",
+                    Activity = "",
+                },
+                new Role()
+                {
+                    Logo = "",
+                    CompanyName = "Instagram",
+                    Position = "Product designer",
+                    Duration = "Full time",
+                    JobId = "123123123",
+                    Status = "Round 2 interview",
+                    Activity = "",
+                },
+                new Role()
+                {
+                    Logo = "",
+                    CompanyName = "Omada Health",
+                    Position = "UX/UI designer",
+                    Duration = "Full time",
+                    JobId = "123123123",
+                    Status = "Phone interview",
+                    Activity = "",
+                },
+                new Role()
+                {
+                    Logo = "",
+                    CompanyName = "Salesforce",
+                    Position = "Visual designer",
+                    Duration = "12 months",
+                    JobId = "123123123",
+                    Status = "Zoom call",
+                    Activity = "",
                 }
             };
         }
--- CRM.Library/Entities/Role.cs
+++ CRM.Library/Entities/Role.cs
@@ -7,30 +7,14 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace CRM.Library.Entities
 {
-    public partial class Role : ObservableObject
+    public class Role
     {
-        [ObservableProperty]
-        public string? logo;
-        [ObservableProperty]
-        public string? companyName;
-        [ObservableProperty]
-        public string? position;
-        [ObservableProperty]
-        public string? duration;
-        [ObservableProperty]
-        public string? jobId;
-        [ObservableProperty]
-        public string? status;
-        [ObservableProperty]
-        public string? activity;
-
-        //public Role(string companyName, string position, string duration, string jobId,string status, string activity = "")
-        //{
-        //    this.companyName = companyName;
-        //    this.position = position;
-        //    this.duration = duration;
-        //    this.jobId = jobId;
-        //    this.activity = activity;
-        //}
+        public string? Logo { get; set; }   
+        public string? CompanyName { get; set; }
+        public string? Position { get; set; }  
+        public string? Duration { get; set; }  
+        public string? JobId { get; set; }  
+        public string? Status { get; set; }  
+        public string? Activity { get; set; }  
     }
 }
