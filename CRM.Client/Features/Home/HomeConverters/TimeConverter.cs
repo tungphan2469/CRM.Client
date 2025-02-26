031a78298a3337f81fb452a6bd99b33fc68d44a2
tungtv1 <tungtv1@newwave.vn>
2025-02-26 16:41:26 +0700

    update

---
+++ CRM.Client/Features/Home/HomeConverters/TimeConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Client.Features.Home.HomeConverters
+{
+    public class TimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dateTime)
+            {
+                return dateTime.ToString("MMMM dd, yyyy");
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DateTime.TryParse(value?.ToString(), out DateTime date) ? date : DateTime.Now;
+        }
+    }
+}
